f3c9d4d3fed5d20a032394f5275bd5cd57af8f98
EscapeFromJava <reske.artur@gmail.com>
2021-11-01 02:44:22 +0300

    Добавьте файлы проекта.

---
+++ Work19_8queens/main.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string coordsToString(int x);
+const int SIZE = 8;
+bool checkByQueen(int x, int y, int Qx, int Qy);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+   
+    int row[SIZE] = { 0 }; //ряды
+    int col[SIZE] = { 0 }; //столбцы
+    int vol = 0;
+    while (vol != 8) {
+        for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    int count = 0;
+                    for (int k = 0; k < SIZE; k++) {
+                        if ((checkByQueen(row[k], col[k], i, j)) == false) {
+                            count++;
+                            if (count == SIZE) {
+                                row[i] = i;
+                                col[i] = j;
+                                vol++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        int row[SIZE] = { 0 };
+        int col[SIZE] = { 0 };
+    }
+    
+
+    for (int i = 0; i < SIZE; i++) {
+        cout << coordsToString(row[i]) <<col[i]+1<< endl;
+    }
+    return 0;
+}
+
+string coordsToString(int x)
+{
+    char c = 'A' + x;
+    string s = "";
+    s += c;
+    return s;
+}
+
+bool checkByQueen(int x, int y, int Qx, int Qy)
+{
+    return (x == Qx || y == Qy || abs(Qy - y) == abs(Qx - x));
+}
+
