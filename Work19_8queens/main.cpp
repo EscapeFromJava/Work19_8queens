005ef44e10983c01df4f09180b481a54240cb7b5
EscapeFromJava <reske.artur@gmail.com>
2021-11-01 03:05:15 +0300

    v 1.1

--- Work19_8queens/main.cpp
+++ Work19_8queens/main.cpp
@@ -14,11 +14,12 @@ int main()
     int row[SIZE] = { 0 }; //ряды
     int col[SIZE] = { 0 }; //столбцы
     int vol = 0;
-    while (vol != 8) {
         for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
+                   
                     int count = 0;
                     for (int k = 0; k < SIZE; k++) {
+                       
                         if ((checkByQueen(row[k], col[k], i, j)) == false) {
                             count++;
                             if (count == SIZE) {
@@ -31,12 +32,8 @@ int main()
                     }
                 }
             }
-        int row[SIZE] = { 0 };
-        int col[SIZE] = { 0 };
-    }
-    
 
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i <= vol; i++) {
         cout << coordsToString(row[i]) <<col[i]+1<< endl;
     }
     return 0;
